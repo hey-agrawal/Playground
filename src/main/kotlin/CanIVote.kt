8eb053a3c9832a28ff882b50a35e9413fc254de9
hey-agrawal <a.prasoon23@gmail.com>
2022-10-21 23:36:46 +0530

    Can I Vote program added. Here I used if else to check whether you can vote or not.

---
+++ src/main/kotlin/CanIVote.kt
@@ -0,0 +1,35 @@
+fun main() {
+
+    println("Enter your age")
+
+    val age = readln().toInt()
+
+    if (age >= 18){
+        println("You can Vote!")
+    } else {
+        println("You can not Vote")
+    }
+
+}
+
+
+/*
+*
+*  1. Print the msg.
+*  2. Get the Int input.
+*  3. Store the result
+*  4. check the result
+*  5. Print the result
+*
+* */
+
+
+/*
+*
+*  1
+*  23
+*  456
+*  78910
+*
+*
+* */
