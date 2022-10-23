20890975a3d4c77cc6477a31cb4d1aa241d2414a
Prasoon Agrawal <a.prasoon23@gmail.com>
2022-10-23 22:12:19 +0530

    Check Number. In this Program i used .length for check Number

---
+++ src/main/kotlin/CheckNumberByLength.kt
@@ -0,0 +1,13 @@
+fun main() {
+
+    println("Enter number")
+
+    val number = readln()
+
+    if (number.length>9 && number.length<11) {
+    println("number is correct")
+    }
+    else{
+        println("number is incorrect")
+    }
+}
