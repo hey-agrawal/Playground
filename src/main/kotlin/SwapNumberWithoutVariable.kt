cd392b8951ab0666505603dfe82ccdb7723e525f
Prasoon Agrawal <a.prasoon23@gmail.com>
2022-11-03 10:53:16 +0530

    Greater number Use Function for  finding Greater Numbeer

---
+++ src/main/kotlin/SwapNumberWithoutVariable.kt
@@ -0,0 +1,17 @@
+fun main() {
+
+    var first = 12.0f
+    var second = 24.5f
+
+    println("--Before swap--")
+    println("First number = $first")
+    println("Second number = $second")
+
+    first = first - second
+    second = first + second
+    first = second - first
+
+    println("--After swap--")
+    println("First number = $first")
+    println("Second number = $second")
+}
