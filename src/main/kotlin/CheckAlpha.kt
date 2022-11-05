2b529d11bf5ddd60f9b239bac27d0c481d180700
Prasoon Agrawal <a.prasoon23@gmail.com>
2022-11-05 19:08:40 +0530

    Check Alphabhet by iff else Statement

---
+++ src/main/kotlin/CheckAlpha.kt
@@ -0,0 +1,9 @@
+fun main() {
+
+    val c = '*'
+
+    if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+        println("$c is an alphabet.")
+    else
+        println("$c is not an alphabet.")
+}
