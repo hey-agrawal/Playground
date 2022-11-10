c2bf4c1673c2c556b916a780f7cce8c14ba8fe9a
Prasoon Agrawal <a.prasoon23@gmail.com>
2022-11-10 19:05:38 +0530

    Use Break

---
+++ src/main/kotlin/CheckAlphabet.kt
@@ -0,0 +1,8 @@
+fun main() {
+
+    val ch = 'i'
+
+    val vowelConsonant = if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') "vowel" else "consonant"
+
+    println("$ch is $vowelConsonant")
+}
