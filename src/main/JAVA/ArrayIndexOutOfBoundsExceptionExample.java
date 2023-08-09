19dd4ec80902c1a81c8012406129752cb1f5f34a
Prasoon Agrawal <a.prasoon23@gmail.com>
2023-08-09 20:52:39 +0530

    Diagonal 2D Array print

--- src/main/JAVA/ArrayIndexOutOfBoundsExceptionExample.java
+++ src/main/JAVA/ArrayIndexOutOfBoundsExceptionExample.java
@@ -1,15 +1,15 @@
-public class ArrayIndexOutOfBoundsExceptionExample {
-
-        public static void main(String[] args) {
-            int[] numbers = { 1, 2, 3 };
-
-            try {
-                // Attempt to access an index that is out of bounds
-                int fourthNumber = numbers[3];
-            } catch (ArrayIndexOutOfBoundsException e) {
-                // Handle the exception by printing an error message
-                System.out.println("Error: Index out of bounds");
-            }
-        }
-    }
-    //write a programe in java to demonstrate try catch and finally
+//public class ArrayIndexOutOfBoundsExceptionExample {
+//
+//        public static void main(String[] args) {
+//            int[] numbers = { 1, 2, 3 };
+//
+//            try {
+//                // Attempt to access an index that is out of bounds
+//                int fourthNumber = numbers[3];
+//            } catch (ArrayIndexOutOfBoundsException e) {
+//                // Handle the exception by printing an error message
+//                System.out.println("Error: Index out of bounds");
+//            }
+//        }
+//    }
+//    //write a programe in java to demonstrate try catch and finally
