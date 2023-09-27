892a02ffa417dfe1346ecbb327892756a311953a
Prasoon Agrawal <a.prasoon23@gmail.com>
2023-09-27 14:55:41 +0530

    Swap Two Numbers

---
+++ src/main/gfg/SwapTwoNumbers.java
@@ -0,0 +1,19 @@
+public class SwapTwoNumbers {
+    public static void main(String[] args)
+    {
+        int a,b,temp;
+        a = 10;
+        b = 15;
+        System.out.println(a);
+        System.out.println(b);
+
+        temp = a;
+        a =b;
+        b =temp;
+
+        System.out.println(a);
+        System.out.println(b);
+
+
+    }
+}
